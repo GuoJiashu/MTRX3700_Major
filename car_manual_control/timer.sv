61cfdb31af24360251af50379fcf636af87180f4
Guojiashu <jacob1365826588@163.com>
2024-10-20 17:11:10 +1100

    timer

--- car_manual_control/timer.sv
+++ car_manual_control/timer.sv
@@ -1,27 +1,20 @@
 module timer #(parameter TIMER_PERIOD = 2, CLK_FREQUENCY = 50_000_000)(
-	input clk,
-	input reset,
-	
-	output timer_triggered
+    input clk,
+    input reset,
+    output timer_triggered
 );
-	int clk_counts;
-	int second_counts = TIMER_PERIOD;
-	
-	always_ff @(posedge clk)begin
-		if (reset) begin
-			clk_counts <= 0;
-			second_counts <= 0;
-		end
-		else begin
-			if (clk_counts == CLK_FREQUENCY-1) begin
-				if (second_counts < TIMER_PERIOD-1) begin
-					second_counts += 1;
-					clk_counts <= 0;
-				end
-			end
-			else clk_counts += 1;
-		end
-	end
-	
-	assign timer_triggered = (second_counts == TIMER_PERIOD-1);
-endmodule 
+    int total_counts;
+    localparam TOTAL_CYCLES = TIMER_PERIOD * CLK_FREQUENCY;
+
+    always_ff @(posedge clk) begin
+        if (reset) begin
+            total_counts <= 0;
+        end else if (total_counts == TOTAL_CYCLES - 1) begin
+            total_counts <= 0;
+        end else begin
+            total_counts <= total_counts + 1;
+        end
+    end
+
+    assign timer_triggered = (total_counts == TOTAL_CYCLES - 1);
+endmodule
