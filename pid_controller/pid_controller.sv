c5471490a955aa20cc50dfacce5e26f30feb0f19
YKanLuo <yluo3177@uni.sydney.edu.au>
2024-10-21 21:02:54 +1100

    fixed pid cpntroller module

--- pid_controller/pid_controller.sv
+++ pid_controller/pid_controller.sv
@@ -1,45 +1,43 @@
 module pid_controller #(
-    parameter integer Kp = 1,         
-    parameter integer Ki = 0,         
-    parameter integer Kd = 0          
+   parameter integer K_P = 18,   // 放大比例系数
+   parameter integer K_I = 10,   // 放大积分系数
+   parameter integer K_D = 10    // 放大微分系数
 )(
-    input logic clk,                  
-    input logic rst,                  
-    input logic signed [31:0] error, 
-    input logic error_ready,          
-    input logic [3:0] speed_level,    // 速度级别0-9
-    output logic signed [15:0] control_output  
+   input logic clk,                  
+   input logic rst,                  
+   input logic signed [31:0] error,  
+   input logic error_ready,          
+   output logic signed [31:0] controller_output  
 );
 
-    // 定义中间变量
-    logic signed [31:0] error_prev;   // 前一个时刻的误差
-    logic signed [31:0] integral;     // 积分项
-    logic signed [15:0] derivative;   // 微分项
-    logic signed [15:0] proportional; // 比例项
-    logic signed [15:0] control_signal; // PID控制信号
+   // 定义中间变量
+   logic signed [31:0] error_prev;   // 前一个时刻的误差
+   logic signed [31:0] I;            // 积分项
+   logic signed [31:0] D;            // 微分项
+   logic signed [31:0] P;            // 比例项
+	
+   always_ff @(posedge clk or posedge rst) begin
+      if (rst) begin
+         error_prev <= 32'd0;
+         P <= 32'd0;
+         I <= 32'd0;
+         D <= 32'd0;
+      end else if (error_ready) begin
+         // P
+         P <= K_P * error;  // 放大比例系数
 
-    always_ff @(posedge clk or posedge rst) begin
-        if (rst) begin
-            error_prev <= 16'd0;
-            integral <= 32'd0;
-            control_output <= 16'd0;
-        end else if (error_ready) begin
-            // P
-            proportional <= Kp * error;
+         // I (可以考虑在此处加上限/下限)
+         I <= I + error;
 
-            // I
-            integral <= integral + error;
+         // D
+         D <= error - error_prev;
 
-            // D
-            derivative <= error - error_prev;
+         // Update
+         error_prev <= error;
+      end
+   end
+	
+   // 将输出控制在 0.1 ~ 1.0 (100 ~ 1000) 之间, 最终输出时再缩小范围
+   assign controller_output = (P + I*K_I + D*K_D) / 264;  // 缩小到0到10
 
-            // Update
-            error_prev <= error;
-
-            // PID输出
-            control_signal <= (proportional + Ki * integral + Kd * derivative);
-
-            control_output <= control_signal * (speed_level + 1); //？？这里不太确定
-        end
-    end
 endmodule
